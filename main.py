5213f7e6d2cbefe05241a1a831f8f3c84cdfc3b9
vladkaGG <vladlenborgardt@gmail.com>
2021-03-22 01:22:37 +0300

    update api

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
+from models import User
+
 
 app = FastAPI()
 
 
 @app.get('/hello')
 async def hello_world():
-    return {'Hello fastapi world'}
+    return {'Hello fast-api world'}
+
+
+@app.post('/user')
+async def create_user(user: User):
+    return {"request body": user}
+
+
+@app.get('/user')
+async def get_user_validation(password: str):
+    return {"query parameter": password}
+
+
+@app.get('/user/{id}')
+async def get_user_by_id(id: str):
+    return {"query id": id}
+
+
+@app.get('/user/{id}/tasks')
+async def get_user_by_task(id: str, status: str, order: str = 'asc'):
+    return {
+        "pet id": id,
+        "query status": status,
+        "query order": order
+    }
